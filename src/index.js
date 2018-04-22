a4d64d906f8ed007e9e2ef23c48b79e4d19967f0
spencer kyonka <spencer.kyonka@hotmail.ca>
2018-04-22 14:45:45 -0400

    add files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 
 // Create new component that should create some HTML
-const App = function() {
+const App = () => {
   return <div> Hi! </div>;
 }
 
 
 
 // Take component's HTML and put it on the page (in the DOM)
-React.render(App);
+ReactDOM.render(<App />, document.querySelector('.container'));
