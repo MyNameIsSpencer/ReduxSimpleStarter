7e0555367e16fc48a05436dcdca88524063a94af
spencer kyonka <spencer.kyonka@hotmail.ca>
2018-04-23 02:04:33 -0400

    working without video_list

---
+++ src/components/video_list.js
@@ -0,0 +1,13 @@
+{/*
+// import React, { Component } from 'react';
+//
+// const VideoList = () => {
+//   return (
+//     <ul className="col-md-4 list-group">
+//       {this.props.video.length}
+//     </ul>
+//   );
+// }
+//
+// export default VideoList;
+*/}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+
+const API_KEY = 'AIzaSyC5ossytDROMvCH5bp3TYrNvJdVqgnaKBg';
 
 
-// Create new component that should create some HTML
-const App = () => {
-  return <div> Hi! </div>;
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { videos: [] };
+
+    YTSearch({ key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+   }
+
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        {/*// <VideoList videos={this.state.videos} />*/}
+      </div>
+    );
+  }
 }
-
 
 
 // Take component's HTML and put it on the page (in the DOM)
