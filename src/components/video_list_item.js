4dbc4d493b1aa9fc4f8830bcaac1349809fdf1b4
spencer kyonka <spencer.kyonka@hotmail.ca>
2018-05-11 21:01:03 -0400

    start video list item

---
+++ src/components/video_list_item.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+{/*  putting  video here  V   can replace the props.video line*/}
+const VideoListItem = ({video}) => {
+  {/*const video = props.video*/}
+  return <li>Video</li>
+};
+
+export default VideoListItem;
